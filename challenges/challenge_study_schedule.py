815962ef502fa0ec63bf15db56d2ade36e0e76c6
nathan crevelin <neitass.music@gmail.com>
2023-09-20 19:37:09 -0300

    fix: erros

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,16 @@
 def study_schedule(permanence_period, target_time):
+    """Faça o código aqui."""
     if target_time is None or not isinstance(target_time, int) or target_time < 0:
         return None
 
     student_count = 0
 
     for period in permanence_period:
-        if not isinstance(period, tuple) or len(period) != 2 or not isinstance(period[0], int) or not isinstance(period[1], int) or period[0] < 0 or period[1] < 0:
+        if not isinstance(period, tuple) or len(period) != 2:
+            return None
+        if not isinstance(period[0], int) or not isinstance(period[1], int):
+            return None
+        if period[0] < 0 or period[1] < 0:
             return None
 
         start, end = period
