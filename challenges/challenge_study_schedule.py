62e8e4292c1318eee298411f99a744ba1c9291b4
nathan crevelin <neitass.music@gmail.com>
2023-09-20 20:35:46 -0300

    fix: refactor study_schedule to (0)n

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,37 +1,21 @@
-def validate_target_time(target_time):
-    """Validate target_time."""
+def study_schedule(presence_period, target_time):
     if target_time is None:
-        return False
-    return True
-
-
-def validate_period(period):
-    """Validate period."""
-    if not isinstance(period, tuple) or len(period) != 2:
-        return False
-    if not isinstance(period[0], int) or not isinstance(period[1], int):
-        return False
-    if period[0] < 0 or period[1] < 0:
-        return False
-    return True
-
-
-def study_schedule(permanence_period, target_time):
-    """
-    Receives a list.
-    """
-    if not validate_target_time(target_time):
         return None
 
-    student_count = 0
+    current_count = 0
 
-    for period in permanence_period:
-        if not validate_period(period):
+    for entry in presence_period:
+        entry_start, entry_end = entry
+
+        if (
+            entry_start is None
+            or entry_end is None
+            or not isinstance(entry_start, (int, float))
+            or not isinstance(entry_end, (int, float))
+        ):
             return None
 
-        start, end = period
+        if entry_start <= target_time <= entry_end:
+            current_count += 1
 
-        if start <= target_time <= end:
-            student_count += 1
-
-    return student_count
+    return current_count
