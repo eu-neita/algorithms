b420b6f04ec801bf6a09144e267a386297a9f9b3
nathan crevelin <neitass.music@gmail.com>
2023-09-20 19:16:34 -0300

    feat: study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None or not isinstance(target_time, int) or target_time < 0:
+        return None
+    
+    student_count = 0
+
+    for period in permanence_period:
+        # Verifica se o período é válido
+        if not isinstance(period, tuple) or len(period) != 2:
+            return None
+
+        start, end = period
+
+        if start <= target_time <= end:
+            student_count += 1
+
+    return student_count
