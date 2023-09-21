743099aa1a9eaedd312f82fe1f9fcd603599b9ce
nathan crevelin <neitass.music@gmail.com>
2023-09-20 22:31:43 -0300

    feat: find duplicated num function

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,12 @@
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+    set_nums = set()
+
+    for num in nums:
+        if type(num) != int or num < 0:
+            return False
+        if num in set_nums:
+            return num
+        set_nums.add(num)
+    return False
