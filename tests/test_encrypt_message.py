e15be54fa3f17584bec284e439ffd5b5b6ed2d7a
nathan crevelin <neitass.music@gmail.com>
2023-09-20 21:14:52 -0300

    feat: make tests to encrypt_message

--- tests/test_encrypt_message.py
+++ tests/test_encrypt_message.py
@@ -5,31 +5,31 @@ from challenges.challenge_encrypt_message import encrypt_message
 def test_encrypt_message_odd_key():
     message = "hello_world"
     key = 3
-    assert encrypt_message(message, key) == "leh_dlrow_ol"
+    assert encrypt_message(message, key) == "wor_olldleh"
 
 
 def test_encrypt_message_even_key():
     message = "hello_world"
     key = 4
-    assert encrypt_message(message, key) == "dlrow_o_lleh"
+    assert encrypt_message(message, key) == "olleh_dlrow"
 
 
-def test_encrypt_message_invalid_type():
-    message = 123
-    key = 3
+def test_encrypt_message_invalid_type_key():
+    message = "hello_world"
+    key = "invalid_key"  # Tipo de chave inválido
     with pytest.raises(TypeError):
         encrypt_message(message, key)
 
 
 def test_encrypt_message_negative_key():
     message = "hello_world"
-    key = -2
+    key = -2  # Chave negativa
     assert encrypt_message(message, key) == "dlrow_olleh"
 
 
 def test_encrypt_message_large_key():
     message = "hello_world"
-    key = 100
+    key = 100  # Chave maior que o comprimento da mensagem
     assert encrypt_message(message, key) == "dlrow_olleh"
 
 
