40e6aa24e753fccfa37b627c69a4ba088de5cecc
nathan crevelin <neitass.music@gmail.com>
2023-09-20 21:47:30 -0300

    fix: test to attempt new file in 1 function

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,17 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    """tests encrypt_message"""
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("hello_world", "invalid_key")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 12)
+
+    assert encrypt_message("hello_world", 4) == "dlrow_o_lleh"
+
+    assert encrypt_message("hello_world", 3) == "leh_dlrow_ol"
+
+    assert encrypt_message("abc", 100) == "cba"
